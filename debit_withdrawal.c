436b1885678b0e08682a3537f03b1bb7bed5b12c
Kashishturan <kashishturan884@gmail.com>
2024-05-30 23:44:25 +0530

    k

---
+++ debit_withdrawal.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+// #include<mysql/mysql.h>
+int main(){
+    float deposit, withdrawal, bal;
+    // char D , W; 
+printf("Welcome to workd of transactions !!");
+char option;
+printf("Enter your option deposit or withrawal the money. For deposit enter D and for withdrawal enter W. ");
+scanf(" %c", &option);
+if (option == 'D' || option =='d'){
+    printf("Enter the amount you wish to deposit.");
+    scanf("%f",&deposit);
+    bal = bal + deposit;
+    printf("\nYour current balance is : %f",bal);
+}
+else if (option=='W' || option == 'w')
+{
+ printf("Enter the amount you wish to withdraw. ");
+    scanf("%f",&withdrawal);
+    bal = bal - withdrawal;
+    printf("Your current balance is : %f",bal);   
+    }
+
+else{
+    printf("No money deposisted or withdrawal.");
+    printf("\nYour balance is %f",bal);
+}
+return 0;
+}
