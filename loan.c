68e194bbd731d025f7a419cd340162cb442955ee
Kashishturan <kashishturan884@gmail.com>
2024-06-04 17:48:01 +0530

    Loan basic

--- loan.c
+++ loan.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main(){
     printf("WElcome to Loan Services of our bank");
+    printf("Kindly login to apply for loans");//if user login
+    printf("Following are the types of loan available: \n 1. Home Loan \n 2. Gold Loan \n 3. Personal Loan \n 4. Education Loan ");
+    int opt ;
+    printf("For home loan type 1 . \n For Gold loan type 2 \n For Personal loan type 3 \n For Education loan type 4 ");
+    printf("\n Enter your choice: ");
+    scanf("%d",&opt);
+
 
 return 0;
 }
